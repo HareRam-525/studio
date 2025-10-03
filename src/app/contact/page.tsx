5632dbc735102c65b1c8866be1768611e0b3b4c5
HareRam-525 <saikrishnacharan108@gmail.com>
2025-10-03 17:49:16 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -27,37 +27,29 @@ export default function ContactPage() {
                 <div className="flex items-start gap-4">
                   <MapPin className="mt-1 h-5 w-5 flex-shrink-0 text-accent" />
                   <span>
-                    123 Event Lane, Celebration City, 456789, Wonderland
+                    Beside Venkatampalli Road, Near Bolikonda Ranganatha Swamy Temple, 515401, Jakkalacheruvu , Andhra Pradesh
                   </span>
                 </div>
                 <div className="flex items-start gap-4">
                   <Phone className="mt-1 h-5 w-5 flex-shrink-0 text-accent" />
-                  <a href="tel:+911234567890" className="hover:text-accent">
-                    +91 12345 67890
+                  <a href="tel:+918886125144" className="hover:text-accent">
+                    +91  88861-25144
                   </a>
                 </div>
                 <div className="flex items-start gap-4">
                   <Mail className="mt-1 h-5 w-5 flex-shrink-0 text-accent" />
                   <a
-                    href="mailto:contact@bkrconventions.com"
+                    href="mailto:bkrconventionhall.jkc@gmail.com"
                     className="hover:text-accent"
                   >
-                    contact@bkrconventions.com
+                    www.bkrconventionhall.jkc@gmail.com
                   </a>
                 </div>
               </div>
             </div>
             <div className="overflow-hidden rounded-xl border">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.521260322283!2d106.819561314769!3d-6.194741395515038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5d2e764b12d%3A0x3d2c562e01528682!2sNational%20Monument!5e0!3m2!1sen!2sid!4v1620029511214!5m2!1sen!2sid"
-                width="100%"
-                height="350"
-                style={{ border: 0 }}
-                allowFullScreen={true}
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-                title="B.K.R Convention Hall Location"
-              ></iframe>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2705.2493507571476!2d77.72055497248593!3d15.110958184157372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb697aadbaabc65%3A0x37af30aebff9252c!2sB.K.R%20Convention%20Hall!5e0!3m2!1sen!2sin!4v1759493929346!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+              </iframe>
             </div>
           </MotionDiv>
           <MotionDiv>
