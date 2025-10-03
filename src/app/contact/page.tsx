722ebc8b2316dda044658b349a1fb74da96c26c5
HareRam-525 <saikrishnacharan108@gmail.com>
2025-10-03 17:55:42 +0530

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -48,8 +48,16 @@ export default function ContactPage() {
               </div>
             </div>
             <div className="overflow-hidden rounded-xl border">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2705.2493507571476!2d77.72055497248593!3d15.110958184157372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb697aadbaabc65%3A0x37af30aebff9252c!2sB.K.R%20Convention%20Hall!5e0!3m2!1sen!2sin!4v1759493929346!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
-              </iframe>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2705.2493507571476!2d77.72055497248593!3d15.110958184157372!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb697aadbaabc65%3A0x37af30aebff9252c!2sB.K.R%20Convention%20Hall!5e0!3m2!1sen!2sin!4v1759494240055!5m2!1sen!2sin"
+                width="100%"
+                height="350"
+                style={{ border: 0 }}
+                allowFullScreen={true}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="B.K.R Convention Hall Location"
+              ></iframe>
             </div>
           </MotionDiv>
           <MotionDiv>
